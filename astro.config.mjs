331f4fbb4d5c38a5d534263701e3c150f5c6f939
Dany720402 <tremblaydany81@outlook.com>
2024-12-31 16:33:54 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+//import { defineConfig } from "astro/config";
+//import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+//export default defineConfig({
+//  integrations: [tailwind()],
+//});
+
+import { defineConfig } from 'astro/config';
+
 export default defineConfig({
-  integrations: [tailwind()],
+  base: '/astro-portfolio/', // Remplacez "nom-du-repo" par le nom de votre dépôt
+  output: 'static',
 });
